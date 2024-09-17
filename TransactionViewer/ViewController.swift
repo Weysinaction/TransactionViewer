f6bfa296318738555f5950e20b43d1c5e5fa1354
LipatovKE <lipatovke@BRY01-M00001.fc.uralsibbank.ru>
2024-09-17 16:33:43 +0300

    init repo

---
+++ TransactionViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionViewer
+//
+//  Created by Липатов Константин Евгеньевич on 17.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
